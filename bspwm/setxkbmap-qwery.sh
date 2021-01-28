3748e888667294488500ed205cf0d5208e6d1b11
demonlord1997 <2273893094@qq.com>
2021-01-28 12:12:00 +0800

    init

---
+++ bspwm/setxkbmap-qwery.sh
@@ -0,0 +1,5 @@
+#/bin/sh
+#setxkbmap us -option -option caps:swapescape
+
+setxkbmap -layout us -option ctrl:nocaps
+xcape -e 'Control_L=Escape'
---
+++ zsh/aliases.zsh
@@ -0,0 +1,18 @@
+alias gz="cd ~/.config/zsh"
+alias gc="cd ~/.config"
+alias gn="cd ~/.config/nvim"
+alias gi="cd ~/.config/i3"
+alias gd="cd ~/Desktop"
+alias gw="cd ~/wallpapers"
+alias g/="cd /"
+alias c="clear"
+alias mk="mkdir"
+alias vim="nvim"
+alias v="vim"
+alias n="neofetch"
+alias ra="ranger"
+alias scc="python ~/usr_file/script/SuperClearCR.py"
+alias google-chrome-stable='google-chrome-stable --proxy-server="127.0.0.1:45107"'
+alias usbl='source ~/usr_file/script/loadusb.sh'
+alias usbr='source ~/usr_file/script/removeusb.sh'
+
